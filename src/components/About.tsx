9a8dcbc98d21b9331b516114923811d3b8973077
Keshav Sharma <keshavsharmao210@gmail.com>
2025-06-22 17:30:24 +0530

    Added full project structure and files

---
+++ src/components/About.tsx
@@ -0,0 +1,20 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          Hi, I’m Keshav Sharma, a passionate Front-End Developer with hands-on
+          experience in building responsive, user-friendly web applications. I
+          specialize in HTML, CSS, JavaScript, Bootstrap, and React.js, and I’m
+          always excited to bring creative ideas to life through clean code and
+          modern design.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
